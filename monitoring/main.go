3fefe3cb0908a664906efea18c2ada19b69ff259
Sam Zagrobelny <strangesast@gmail.com>
2020-01-29 21:47:41 +0000

    stoopid but working?

--- monitoring/main.go
+++ monitoring/main.go
@@ -15,7 +15,8 @@ type result struct {
 	Components []string `xml:"Streams>DeviceStream>ComponentStream>Samples>Position"`
 }
 
-type position struct {
+type value struct {
+	XMLName   xml.Name
 	Timestamp string `xml:"timestamp,attr"`
 	Sequence  int    `xml:"sequence,attr"`
 	Value     string `xml:",chardata"`
@@ -31,6 +32,72 @@ func getXMLAttr(el xml.StartElement, key string) string {
 		}
 	}
 	return ""
+}
+
+func outer(ch1 chan xml.Token, decoder *xml.Decoder) chan interface{} {
+	ch0 := make(chan interface{})
+	// defer close(ch0)
+	go func() {
+		var t xml.Token
+		for {
+			t = <-ch1
+			if t == nil {
+				break
+			}
+			ch2 := filter(ch1, "ComponentStream") // get children of ComponentStream, start with parent
+			t = <-ch2
+			if t != nil { // if parent found
+				name := getXMLAttr(t.(xml.StartElement), "name") // get parent name attr
+				fmt.Printf("name: '%s'\n", name)
+				ch3 := filter(ch2, "Samples") // get any children with parent "Sample"
+				t = <-ch3
+				if t != nil { // Sample found
+					// could get sample attributes
+					for {
+						t = <-ch3
+						if t == nil {
+							break
+						}
+						if se, ok := t.(xml.StartElement); ok {
+							fmt.Println(se.Name.Local)
+							// var val value
+							// err := decoder.DecodeElement(&val, &se)
+							// if err != nil {
+							// 	fmt.Println(err)
+							// }
+						}
+					}
+				}
+			}
+		}
+	}()
+	return ch0
+}
+
+func filter(ch0 chan xml.Token, name string) chan xml.Token {
+	var t xml.Token
+	ch1 := make(chan xml.Token)
+	// defer close(ch1)
+	go func() {
+		for {
+			t = <-ch0
+			if t == nil {
+				break
+			}
+			if se, ok := t.(xml.StartElement); ok && se.Name.Local == name {
+				break
+			}
+		}
+		for t != nil {
+			if ee, ok := t.(xml.EndElement); ok && ee.Name.Local == name {
+				break
+			}
+			ch1 <- t
+			t = <-ch0
+		}
+		ch1 <- nil
+	}()
+	return ch1
 }
 
 func main() {
@@ -66,6 +133,9 @@ func main() {
 
 	decoder := xml.NewDecoder(resp.Body)
 
+	var state int = 0
+	var name string
+
 	for {
 		t, err := decoder.Token()
 		if err == io.EOF {
@@ -78,48 +148,20 @@ func main() {
 			break
 		}
 		if se, ok := t.(xml.StartElement); ok && se.Name.Local == "ComponentStream" {
-			name := getXMLAttr(se, "name")
-			fmt.Printf("\"%s\"\n", name)
-			t, err := decoder.Token()
-			if err == io.EOF {
-				break
+			state = 1
+			name = getXMLAttr(se, "name")
+		} else if se, ok := t.(xml.StartElement); ok && se.Name.Local == "Samples" {
+			state = 2
+		} else if se, ok := t.(xml.StartElement); ok && state == 2 {
+			var val value
+			err := decoder.DecodeElement(&val, &se)
+			if err == nil {
+				fmt.Println(name, val.XMLName.Local, val.Timestamp, val.Sequence, val.Value)
 			}
-			if err != nil {
-				log.Fatal(err)
-			}
-			if t == nil {
-				break
-			}
-			if ee, ok := t.(xml.EndElement); ok && ee.Name.Local == "ComponentStream" {
-				break
-			}
-			//if se.Name.Local == "MTConnectStreams" {
-			//	var p Result
-			//	decoder.DecodeElement(&p, &se)
-			//	for i, val := range p.Components {
-			//		fmt.Printf("%d - %s\n", i, val)
-			//	}
-			//}
-			// for elem in "just ComponentStream"s:
-			//   get elem.Attr["name"]
-			//   for elem in "just ComponentStream>Samples>*":
-			//     get elem.Tag (i.e. "Position"), timestamp, and value
-			if se.Name.Local == "Samples" {
-
-			}
-
-			// if se.Name.Local == "Position" {
-			// 	// fmt.Println(se)
-			// 	var p position
-			// 	decoder.DecodeElement(&p, &se)
-			// 	fmt.Println(p)
-			// 	// for _, val := range se.Attr {
-			// 	// 	if val.Name.Local == "name" {
-			// 	// 		fmt.Printf("name=%s\n", val.Value)
-			// 	// 	}
-			// 	// }
-			// }
-
+		} else if ee, ok := t.(xml.EndElement); ok && ee.Name.Local == "Samples" {
+			state = 1
+		} else if ee, ok := t.(xml.StartElement); ok && ee.Name.Local == "ComponentStream" {
+			state = 0
 		}
 	}
 }
