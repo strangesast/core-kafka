fbe6d6811b72c9fbb6a317fc0192a4f50e467ad8
Sam Zagrobelny <strangesast@gmail.com>
2020-07-03 11:50:28 -0400

    removed protobuf, switched to avro

--- scripts/watch-kafka.sh
+++ scripts/watch-kafka.sh
@@ -2,5 +2,6 @@
 # --from-beginning \
 # --property print.timestamp=true \
 docker run --rm -it --network=host wurstmeister/kafka /opt/kafka/bin/kafka-console-consumer.sh \
+  --property print.key=true \
   --bootstrap-server localhost:9092 \
   --topic ${1:-input}
