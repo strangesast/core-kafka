3dde9b06b60bfa6915a6bc65f2abcf27ad6adfc4
Sam Zagrobelny <strangesast@gmail.com>
2020-07-13 12:08:59 +0000

    tweak

--- connect/entrypoint.sh
+++ connect/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export KAFKA_HOSTS="${KAFKA_HOSTS:-kafka:9092}"
+export KAFKA_HOSTS="${KAFKA_HOSTS:-localhost:9092}"
 export FLUSH_INTERVAL="${FLUSH_INTERVAL:-10000}"
 export DATABASE_URL="${DATABASE_URL:-postgresql://localhost:5432/development}"
 export DATABASE_USER="${DATABASE_USER:-postgres}"
